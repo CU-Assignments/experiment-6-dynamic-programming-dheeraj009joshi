aba850ae9ac1cb363c73fa6fedc172522ddb84a9
dheeraj009joshi <73873399+dheeraj009joshi@users.noreply.github.com>
2025-02-27 11:41:16 +0530

    Add files via upload

---
+++ Solution (70. Climbing Stairs).java
@@ -0,0 +1,18 @@
+//Exp-4 (Problem : #70. Climbing Stairs)
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 1)
+            return 1;
+        if (n == 2)
+            return 2;
+
+        int first = 1, second = 2;
+        for (int i = 3; i <= n; i++) {
+            int temp = first + second;
+            first = second;
+            second = temp;
+        }
+
+        return second;
+    }
+}
